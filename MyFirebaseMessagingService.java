ac65a05f8ad151ff7ab20cb1fabe6ea74a81ee83
ajung <cwhtha@gmail.com>
2018-06-10 14:48:55 +0900

    0610 push function (2.8): send and receive msgs

--- MyFirebaseMessagingService.java
+++ MyFirebaseMessagingService.java
@@ -1,9 +1,13 @@
 package ajung13.github.saimo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "myLog_msg";
@@ -11,5 +15,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage){
         Log.e(TAG, "onMsgReceived called");
+
+        String from = remoteMessage.getFrom();
+        Map<String, String> data = remoteMessage.getData();
+        String contents = data.get("contents");
+        Log.e(TAG, "from : " + from + ", contents : " + contents);
+
+        sendToActivity(getApplicationContext(), from, contents);
+    }
+
+    private void sendToActivity(Context context,String from, String contents){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra("from", from);
+        intent.putExtra("contents", contents);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        context.startActivity(intent);
     }
 }
