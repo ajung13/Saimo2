8d7bcc44756b8cb9615a4b84abc18b3473fd7a70
ajung <cwhtha@gmail.com>
2018-07-08 16:12:09 +0900

    0708 splash completed

--- IntroActivity.java
+++ IntroActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
     SharedPreferences preferences;
@@ -14,7 +19,9 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
         preferences = getSharedPreferences("ajung13.github.saimo", MODE_PRIVATE);
+        setAnimations();
     }
 
     @Override
@@ -35,6 +42,22 @@ public class IntroActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 1000);
+        }, 2500);
+    }
+
+    private void setAnimations(){
+        ImageView iv = findViewById(R.id.intro_img);
+        TextView tv1 = findViewById(R.id.intro_txt1);
+        TextView tv2 = findViewById(R.id.intro_txt2);
+
+        Animation txtAnim = new AlphaAnimation(0.0f, 1.0f);
+        txtAnim.setStartTime(1000);
+        txtAnim.setDuration(1000);
+        tv1.startAnimation(txtAnim);
+        tv2.startAnimation(txtAnim);
+
+        Animation transAnim = new TranslateAnimation(-200, 0, 0, 0);
+        transAnim.setDuration(800);
+        iv.startAnimation(transAnim);
     }
 }
